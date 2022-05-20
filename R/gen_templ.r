a6a06032892167925f7d5bd1f4558bbdf75f8789
BlueGrisGris <emanninen@college.harvard.edu>
2022-05-20 15:27:43 -0400

    terra for importing matchdomain in gen_templ

--- R/gen_templ.r
+++ R/gen_templ.r
@@ -1,11 +1,62 @@
 #' Gen_Raster_Templ()
 #' Creates the raster brick/stack/ netCDF4 structure that will be filled with processed driver data and VPRM NEE output. 
-#' @param domain 
-#' @param lons 
-#' @param lats 
-#' @param times 
+#' @param matchdomain 
+#' @param lons ### NOT IMPLEMENTED 
+#' @param lats ### NOT IMPLEMENTED 
+#' @param times ### NOT IMPLEMENTED 
+#' @param projection ### NOT IMPLEMENTED
+#' @param vpRm_dir
+#' @param outfile ### NOT IMPLEMENTED
+#' @param verbose
 
 #' @export
-gen_templ <- function(domain, lons, lats, times){
-	return("hello!")
-}#end function Gen_Raster_Templ()
+gen_templ <- function(
+		      matchdomain = NULL
+		      , lons = NULL
+		      , lats = NULL
+		      , times = NULL 
+	     	      , xres = NULL
+		      , yres = NULL
+		      , projection = "+proj=longlat"
+		      , vpRm_dir = "."
+		      , outfile
+		      , verbose = F
+		      ){ #start func gen_templ
+
+	if(!is.null(matchdomain)){
+		stop("Specing coordinates is not yet implemented.  Supply matchdomain a file path with domain over which you want to run VPRM")
+	} #end if !is.null matchdomain
+
+	### make sure there is enough info provided to create a template
+	if(!is.null(matchdomain)&( is.null(lons)| is.null(lats)|is.null(times)|is.null(xres)|is.null(yres) ) ){
+		stop("If an example format is not provided via matchdomain, all of lon, lat, times, xres, yres must be provided")
+	} #end if !is.null matchdomain
+#######
+### create template to match a given domain from a file, ie a stilt footprint.  
+#######
+	if(!is.null(matchdomain)){
+
+	if(verbose){
+	print(paste("Attempting to parse",matchdomain,"and create template"))
+	}#end if verbose
+
+	### check if matchdomain is a directory
+	if(length(list.files(matchdomain)!=0)){
+	filenames<- list.files(matchdomain)
+	}#end if(length(list.files(matchdomain)!=0)){
+
+	### read in data to match domain to using terra
+	domain <- terra::rast(matchdomain)
+
+	### TODO: Decide on projection plans
+	### TODO: Look 1 more time for a way to have more than 3 dims in a terra::spatRaster
+	### TODO: either empty domain, or create a new spat raster with same attributes 
+	### TODO: save into init_vpRm() created dir processed
+
+	}#if(!is.null(matchdomain)){
+#######
+### create template from given coordinates
+### TO BE IMPLEMENTED
+#######
+return(NULL)
+}#end func gen_templ
--- tests/testthat/test_gen_templ.r
+++ tests/testthat/test_gen_templ.r
@@ -1,3 +1,16 @@
+tests_dir <- system.file("tests",package="vpRm")
+data_dir <- file.path(tests_dir, "data") 
+stilt_dir <- file.path(data_dir, "stilt_test")
+list.files(stilt_dir )
+
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+matchdomain1 <- file.path(stilt_dir,"by-id","202001310500_-72.1898_42.5331_364","202001310500_-72.1898_42.5331_364_foot.nc")
+matchdomain2 <- file.path(stilt_dir,"by-id","202007210500_-72.1898_42.5331_364","202007210500_-72.1898_42.5331_364_foot.nc")
+matchdomain3 <- file.path(stilt_dir,"by-id","202010101200_-72.1898_42.5331_364","202010101200_-72.1898_42.5331_364_foot.nc")
+matchdomain <- c(matchdomain1,matchdomain2,matchdomain3)
+
+
+
