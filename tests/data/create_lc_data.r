90a989ade26004bc1dd5033c2d5e560d04199a61
BlueGrisGris <emanninen@college.harvard.edu>
2022-05-22 16:55:38 -0400

    created land cover test data

--- tests/data/create_lc_data.r
+++ tests/data/create_lc_data.r
@@ -4,11 +4,12 @@ library(terra)
 tests_dir <- system.file("tests",package="vpRm")
 data_dir <- file.path(tests_dir, "data") 
 
-lc_filename <- file.path("~/Desktop/research/phd/em27_carbfor/vprm/NLCD/LC/nlcd_2019_land_cover_l48_20210604.img")
+# lc_filename <- file.path("~/Desktop/research/phd/em27_carbfor/vprm/NLCD/LC/nlcd_2019_land_cover_l48_20210604.img")
+lc_filename <- file.path("~/Downloads/nlcd_2019_land_cover_l48_20210604/nlcd_2019_land_cover_l48_20210604.img")
 file.exists(lc_filename)
 lc <- rast(lc_filename)
 
-### see extent of test_stilt in lc_crs so that can pick somethign a bit bigger for test_lc 
+### see extent of test_stilt in lc_crs so that can pick somethign a bit bigger for lc_test 
 stilt_dir <- file.path(data_dir, "stilt_test")
 matchdomain1 <- file.path(stilt_dir,"by-id","202001310500_-72.1898_42.5331_364","202001310500_-72.1898_42.5331_364_foot.nc")
 domain <- rast(matchdomain1)
@@ -19,8 +20,12 @@ plot(lc)
 raster_lc <- raster::raster(lc_filename)
 raster::plot(raster_lc)
 
-test_lc <- lc
-ext(test_lc) <- ext(proj_domain)
-test_lc <- crop(lc,proj_domain) 
+lc_test <- terra::crop(lc,ext(proj_domain)*1.5)
+
 plot(proj_domain)
-plot(test_lc)
+plot(lc_test,type= "classes")
+
+lc_test <- aggregate(lc_test,fact = 100, fun="modal")
+
+plot(lc_test,type= "classes")
+writeCDF(lc_test, file.path("~/Downloads", "lc_test.nc"), overwrite=T)
