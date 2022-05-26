b308515afa35fd40e56d2c201e108c5f3ec18f79
BlueGrisGris <emanninen@college.harvard.edu>
2022-05-26 10:17:36 -0400

    fix empty proc_simple_2d bug: wrong double negation on verify input if statements

--- R/proc_simple_2d.r
+++ R/proc_simple_2d.r
@@ -1,11 +1,17 @@
 #' proc_simple_2d
-#' Convert Land cover data to match crs and extent to template
+#' Convert 2d data to match crs and extent to template
 
 #' @param driver (Spat Raster or terra::SpatRaster): SpatRaster or filepath that can be input to terra::Rast for  land cover data file to be processed
 #' @param plate (Spat Raster or terra::SpatRaster): SpatRaster or filepath that can be input to terra::Rast for vpRm template
 
 #' @export 
 proc_simple_2d <- function(driver, plate){
+
+print(length(which(!is.nan(terra::values(driver)))))
+print("################")
+print(class(driver))
+print(class(plate))
+
 ### touch lc driver data 
 if(!class(driver)[[1]] != "SpatRaster"){
 	if(!class(driver)[[1]] != "character"){
@@ -23,12 +29,21 @@ if(!class(plate)[[1]] != "SpatRaster"){
 
 ### TODO: right now domain will always be encompassed by LC, bc LC is CONUS.  This will not be the case forever.
 
+print(length(which(!is.nan(terra::values(driver)))))
+print("################")
 ### TODO: think about the performance of this processing as an excercise
-driver_proj <- terra::project(driver, plate) 
-processed <- terra::crop(driver_proj,terra::ext(plate))
+### reproject then crop
+### TODO: method = near is for land cover data. maybe make method an arg to proc_simple_2d?
+processed <- terra::project(driver, plate, method = "near") 
+### it seems that reproject makes cropping uneccesary
+# processed <- terra::crop(driver_proj,terra::ext(plate))
+
+print(length(which(!is.nan(terra::values(processed)))))
+print("################")
 
 return(processed)
 
 }#end func proc_lc
 
 
+proc_lc <- proc_simple_2d(driver,plate)
--- tests/testthat/test_proc_simple_2d.r
+++ tests/testthat/test_proc_simple_2d.r
@@ -1,16 +1,30 @@
+### TODO: maybe offload this into its own script
 data_dir <- system.file("test_data",package="vpRm",mustWork = T)
 list.files(data_dir)
 lc_filename <- file.path(data_dir, "lc_test.nc") 
 file.exists(lc_filename)
 lc <- terra::rast(lc_filename)
+driver <- lc
 plate_filename <- file.path(data_dir, "plate.nc")
 file.exists(plate_filename)
 plate <- terra::rast(plate_filename)
 
 test_that("does proc_simple_2d work on lc?", {
+
 proc_lc <- proc_simple_2d(lc,plate)
+
+if(F){
+terra::plot(plate[[1]])
+terra::plot(driver)
+terra::plot(processed)
+terra::plot(proc_lc)
+terra::values(proc_lc)
+}
+
 ### nrows and ncols should match, but only 1 layer for land cover and 19 time layers for template
-testthat::expect_equal(c(dim(proc_lc)[c(1,2)],1),c(dim(plate)[c(1,2)],1)   )
+expect_equal( c(dim(proc_lc)[c(1,2)],1), c(dim(plate)[c(1,2)],1) )
+### make sure it has data over the whole domain of plate ### TODO: or provide error and offer proc_interp
+expect_equal( length(which(!is.nan(terra::values(proc_lc)))) , length(terra::values(plate)) )
 }) #end test_that()
 
 ### TODO: test proc_simple_2d on isa data
