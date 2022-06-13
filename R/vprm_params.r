983dc0eeace84f33749f39ac9c7ca97e1083bcfb
BlueGrisGris <emanninen@college.harvard.edu>
2022-06-13 13:42:28 -0400

    save vprm_params dataframe

---
+++ R/vprm_params.r
@@ -0,0 +1,17 @@
+#' VPRM parameters
+#' 
+#' VPRM parameters for different landcover types 
+#' 
+#' @format Data frame with 11 rows and 9 cols. 
+#' \describe{ 
+#' lc      
+#' item{ Tmin }
+#' item{ Topt }
+#' item{ Tmax }
+#' item{ Tlow }
+#' item{ PAR0 }
+#' item{ lambda }
+#' item{ alpha }
+#' item{ beta }
+#' } 
+"vprm_params"
