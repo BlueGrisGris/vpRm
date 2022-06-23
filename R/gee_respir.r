40e9299c487d114676b8e9b7b60b907894c0d61c
BlueGrisGris <emanninen@college.harvard.edu>
2022-06-22 20:52:37 -0400

    consolidate files: gee, respir

--- R/gee_respir.r
+++ R/gee_respir.r
@@ -1,3 +1,12 @@
+### Mahadevan et 2008
+gee <- function(lambda, Tscalar, Pscalar, Wscalar, EVI, PAR, PAR0, lc){
+	GEE <-  (lambda * Tscalar * Pscalar * Wscalar * EVI * PAR )/( (1+PAR)/PAR0 )
+	if(lc == "OTH"){
+		GEE  <- 0
+	}#end if(lc == "OTH"){
+	return(GEE)
+}#end func gee
+
 ### TODO: should this be evi scales?
 respir <- function(tair, alpha, beta, lc, isa, evi){
 	# respir_naive <- function(alpha, tair, beta){
--- tests/testthat/test_gee_respir.r
+++ tests/testthat/test_gee_respir.r
@@ -1,3 +1,8 @@
+test_that("does gee() calc gee correctly?", {
+	expect_equal(0, gee(1,2,3,4,5,6,7,"OTH"))
+	expect_equal(720, gee(1,2,3,4,5,6,7,"DBF"))
+})#end test_that("does gee() calc gee correctly?", {
+
 test_that("does respir calculate the correct respiration values?", {
 	expect_equal(0,respir <- respir(tair = 1, alpha = 2, beta = 3, lc = "OTH", isa = .5, evi = .5))
 	expect_equal(2.5,respir(tair = 1, alpha = 2, beta = 3, lc = "DBF", isa = .5, evi = .5))
