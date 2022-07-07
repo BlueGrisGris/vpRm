54149ae5d91f331857ac6a086149583e25a9a280
BlueGrisGris <emanninen@college.harvard.edu>
2022-07-07 10:12:27 -0400

    haggard parse filenames was a bad idea after all

--- R/parse_filenames.r
+++
@@ -1,7 +0,0 @@
-### TODO: this is all haggard but I don't know a better way 
-
-### parse doy from modis evi filenames downloades through appears
-parse_doy_from_evi <- function(evi_filenames){
-	doy <- substr(evi_filenames, 34, 40)
-	return(as.numeric(doy))
-}#end func
--- tests/testthat/test_parse_filenames.r
+++
@@ -1,4 +0,0 @@
-test_that("does parse_doy_from_evi() return the correct doy?", {
-	evi_filenames <- "MYD13A1.006__500m_16_days_EVI_doy2019361_aid0001.tif" 
-	expect_equal( parse_doy_from_evi(evi_filenames[1]),  2019361)
-})#end test_that("does parse_doy_from_evi() return the correct doy?", {
