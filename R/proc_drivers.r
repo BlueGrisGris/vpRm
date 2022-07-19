6acdbb0448e7df3734e8cc29923f8692dc2c8ec5
BlueGrisGris <emanninen@g.harvard.edu>
2022-07-19 19:52:03 -0400

    fixed time parsing and propogation

--- R/proc_drivers.r
+++ R/proc_drivers.r
@@ -34,7 +34,6 @@ proc_drivers.vpRm <- function(vpRm){
 	lc_proc <- proc_2d(lc,plate)
 	if(vpRm$verbose){Print_Info(lc_proc)}
 	Save_Rast(lc_proc, vpRm$dirs$lc_proc_dir)
-	rm(lc, lc_proc)
 
 	####### process isa
 	if(vpRm$verbose){print("start process impermeability")}
@@ -48,8 +47,9 @@ proc_drivers.vpRm <- function(vpRm){
 	####### process temp
 	if(vpRm$verbose){print("start process temperature")}
 	temp <- terra::rast(vpRm$dirs$temp_dir)
+	time(temp) <- parse_herbie_hrrr_times(vpRm$dirs$temp_dir)
 	if(vpRm$verbose){Print_Info(temp)}
-	temp_proc <- proc_3d(temp,plate)
+	temp_proc <- proc_3d(temp,plate, temp_times)
 	if(vpRm$verbose){Print_Info(temp_proc)}
 	Save_Rast(temp_proc, vpRm$dirs$temp_proc_dir)
 	rm(temp, temp_proc)
@@ -57,6 +57,8 @@ proc_drivers.vpRm <- function(vpRm){
 	####### process par
 	if(vpRm$verbose){print("start process PAR")}
 	par <- terra::rast(vpRm$dirs$par_dir)
+	time(par) <- parse_herbie_hrrr_times(vpRm$dirs$par_dir)
+	if(vpRm$verbose){Print_Info(temp)}
 	if(vpRm$verbose){Print_Info(par)}
 	par_proc <- proc_3d(par,plate)
 	if(vpRm$verbose){Print_Info(par_proc)}
@@ -64,12 +66,20 @@ proc_drivers.vpRm <- function(vpRm){
 	rm(par, par_proc)
 
 	####### process evi
+	evi_scale_factor <- 1e-5
+
 	### TODO: test that evi \in {-1,1}
 	if(vpRm$verbose){print("start process evi")}
 	evi <- terra::rast(vpRm$dirs$evi_dir)
-	if(vpRm$verbose){Print_Info(evi)}
+	time(evi) <- parse_modis_evi_times(vpRm$dirs$evi_dir)
+	if(vpRm$verbose){Print_Info(temp)}
 	evi_proc <- proc_3d(evi,plate, strict_times = F)
-	evi_proc <- evi_proc*1e-4
+	evi_proc <- evi_proc*evi_scale_factor
+	### mask out water which would ruin extrema
+	evi_proc <- terra::mask(evi_proc, lc_proc, maskvalues = 11)
+	### TODO: better (real) ocean mask
+	ocean_cutoff <- .35
+	evi_proc <- terra::mask(evi_proc, evi_proc<ocean_cutoff, maskvalues = 0)
 	if(vpRm$verbose){Print_Info(evi)}
 	Save_Rast(evi_proc, vpRm$dirs$evi_proc_dir)
 
@@ -84,7 +94,7 @@ proc_drivers.vpRm <- function(vpRm){
 	}else{
 		evi_extrema <- terra::rast(vpRm$dirs$evi_extrema_dir)
 		evi_extrema_proc <- proc_2d(evi_extrema,plate)
-		evi_extrema_proc <- evi_extrema_proc*1e-4
+		evi_extrema_proc <- evi_extrema_proc*evi_scale_factor
 	} #end else
 	if(vpRm$verbose){Print_Info(evi_extrema_proc)}
 	Save_Rast(evi_extrema_proc, vpRm$dirs$evi_extrema_proc_dir)
