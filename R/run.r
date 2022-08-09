bd3ba581ef94facae601e764f71aceb6f50becc4
BlueGrisGris8950 <emanninen@g.harvard.edu>
2022-08-09 05:58:18 +0000

    rename more run objects

--- R/run.r
+++ R/run.r
@@ -16,24 +16,24 @@ run.vpRm <- function(vpRm){
 
 plate <- terra::rast(vpRm$dirs$plate_dir)
 
-lc <- terra::rast(vpRm$dirs$lc_proc_dir)
-isa <- terra::rast(vpRm$dirs$isa_proc_dir)
+LC <- terra::rast(vpRm$dirs$lc_proc_dir)
+ISA <- terra::rast(vpRm$dirs$isa_proc_dir)
 
-if( any(dim(lc) != c(dim(plate)[1:2], 1) )){stop(paste("dim lc_proc =", dim(lc), " does not align dim plate =", dim(plate)))}
-if( any(dim(isa) != c(dim(plate)[1:2], 1) )){stop(paste("dim isa_proc =", dim(isa), " does not align dim plate =", dim(plate)))}
+if( any(dim(LC) != c(dim(plate)[1:2], 1) )){stop(paste("dim lc_proc =", dim(LC), " does not align dim plate =", dim(plate)))}
+if( any(dim(ISA) != c(dim(plate)[1:2], 1) )){stop(paste("dim isa_proc =", dim(ISA), " does not align dim plate =", dim(plate)))}
 
 
-temp <- terra::rast(vpRm$dirs$temp_proc_dir)
+TEMP <- terra::rast(vpRm$dirs$temp_proc_dir)
 PAR <- terra::rast(vpRm$dirs$par_proc_dir)
 EVI <- terra::rast(vpRm$dirs$evi_proc_dir)
-if( any(dim(temp) != dim(plate)) ){stop(paste("dim temp_proc =", dim(temp), " does not match dim plate =", dim(plate)))}
-if( any(dim(par) != dim(plate)) ){stop(paste("dim par_proc =", dim(par), " does not match dim plate =", dim(plate)))}
+if( any(dim(TEMP) != dim(plate)) ){stop(paste("dim temp_proc =", dim(TEMP), " does not match dim plate =", dim(plate)))}
+if( any(dim(PAR) != dim(plate)) ){stop(paste("dim par_proc =", dim(PAR), " does not match dim plate =", dim(plate)))}
 if( any(dim(evi) != dim(plate)) ){stop(paste("dim evi_proc =", dim(evi), " does not match dim plate =", dim(plate)))}
 
 EVIextrema <- terra::rast(vpRm$dirs$evi_extrema_proc_dir)
-green <- terra::rast(vpRm$dirs$green_proc_dir)
+GREEN <- terra::rast(vpRm$dirs$green_proc_dir)
 if( any(dim(EVIextrema) != c(dim(plate)[1:2], 2) )){stop(paste("dim EVIextrema_proc =", dim(EVIextrema), " does not align dim plate =", dim(plate)))}
-if( any(dim(green) != c(dim(plate)[1:2], 2) )){stop(paste("dim green_proc =", dim(green), " does not align dim plate =", dim(plate)))}
+if( any(dim(GREEN) != c(dim(plate)[1:2], 2) )){stop(paste("dim green_proc =", dim(GREEN), " does not align dim plate =", dim(plate)))}
 
 vprm_params <- vpRm$params
 
@@ -45,22 +45,22 @@ if(vpRm$verbose){print("read in proc'd data");print(terra::free_RAM())}
 if(vpRm$verbose){print("start collate VPRM params")}
 
 ### TODO: check that we dont need an addtl mask
-lambda <- sum( (lc == vprm_params[,"lc"])*vprm_params[,"lambda"] )
+lambda <- sum( (LC == vprm_params[,"lc"])*vprm_params[,"lambda"] )
 
-Tmin <-  sum( (lc == vprm_params[,"lc"])*vprm_params[,"Tmin"] )
-Tmax <-  sum( (lc == vprm_params[,"lc"])*vprm_params[,"Tmax"] )
+Tmin <-  sum( (LC == vprm_params[,"lc"])*vprm_params[,"Tmin"] )
+Tmax <-  sum( (LC == vprm_params[,"lc"])*vprm_params[,"Tmax"] )
 
-PAR0 <-  sum( (lc == vprm_params[,"lc"])*vprm_params[,"PAR0"] )
+PAR0 <-  sum( (LC == vprm_params[,"lc"])*vprm_params[,"PAR0"] )
 
-alpha <- sum( (lc == vprm_params[,"lc"])*vprm_params[,"alpha"] )
-beta <-  sum( (lc == vprm_params[,"lc"])*vprm_params[,"beta"] )
+alpha <- sum( (LC == vprm_params[,"lc"])*vprm_params[,"alpha"] )
+beta <-  sum( (LC == vprm_params[,"lc"])*vprm_params[,"beta"] )
 
 #############################################
 ### calculate scalars
 #############################################
 if(vpRm$verbose){print("start calculate scalars")}
 
-Tscalar <- Tscalar(temp, Tmin, Tmax)
+Tscalar <- Tscalar(TEMP, Tmin, Tmax)
 
 EVImax <- EVIextrema[[1]]
 EVImin <- EVIextrema[[2]]
@@ -90,14 +90,14 @@ terra::time(GEE) <- terra::time(plate)
 if(vpRm$verbose){print("start apply growing system boundary")}
 ### Set gee to zero outside of growing season
 doy <- lubridate::yday(terra::time(GEE)) 
-green_mask <- (green[[1]] < doy) & (green[[2]] > doy)
+green_mask <- (GREEN[[1]] < doy) & (GREEN[[2]] > doy)
 ### but not for evergreen?
 ### TODO: our test set doesn't have evergreen, so untested
-green_mask[lc == 42] <- 1
+green_mask[LC == 42] <- 1
 GEE <- GEE*green_mask 
 
 ### gee = zero where there is water
-GEE <- GEE * (lc!=11)
+GEE <- GEE * (LC!=11)
 ### just a few pixels come out negative
 ### TODO: send a warning if more than 1%
 GEE <- GEE * (GEE>0)
@@ -114,16 +114,18 @@ terra::writeRaster(GEE, vpRm$dirs$gee, overwrite = T)
 if(vpRm$verbose){print("start calculate respiration")}
 
 RESPIR <- respir(
-	temp
+	TEMP
 	, alpha
 	, beta
-	, lc
-	, isa
+	, LC
+	, ISA
 	, EVI
 )#end respir
 
+browser()
+
 ### respir = zero where there is water
-RESPIR <- RESPIR * (lc!=11)
+RESPIR <- RESPIR * (LC!=11)
 
 terra::time(RESPIR) <- terra::time(plate)
 
