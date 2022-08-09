b63cec1fbcda38da29a7eb374dd92b69cc196d8a
BlueGrisGris <emanninen@g.harvard.edu>
2022-08-09 10:16:13 -0400

    fix herbie fetch, rm 6h forecast

--- fetch_scripts/hrrr_par_temp.py
+++ fetch_scripts/hrrr_par_temp.py
@@ -4,8 +4,8 @@ from herbie import Herbie
 from herbie.tools import fast_Herbie, fast_Herbie_download
 import pandas as pd
 
-start_time = "2019-01-01 00:00"
-#end_time = "2019-01-02 00:00"
+start_time = "2019-08-20 00:00"
+#end_time = "2019-08-20 23:00"
 end_time = "2020-12-31 23:00"
 
 times = pd.date_range(start_time, end_time, freq = "1H")
@@ -15,16 +15,21 @@ HH_par = fast_Herbie_download(
     DATES = times
   , model='hrrr'
   , product='sfc'
-  , fxx=6
+  , fxx=0
   , searchString = "DSWRF"
-  , save_dir = "/n/wofsy_lab2/Users/emanninen/vprm/driver_data/hrrr_par"
+  #, save_dir = "/n/wofsy_lab2/Users/emanninen/vprm/driver_data/hrrr_par"
+  , save_dir = "~/Downloads/par"
 )#end Herbie
 
-#HH_temperature = fast_Herbie_download(
-#    DATES = times
-#  , model='hrrr'
-#  , product='sfc'
-#  , fxx=6
-#  , searchString = "TMP:2 m"
-#  , save_dir = "/n/wofsy_lab2/Users/emanninen/vprm/driver_data/hrrr_temperature"
-#)#end Herbie
+
+HH_temperature = fast_Herbie_download(
+    DATES = times
+  , model='hrrr'
+  , product='sfc'
+  , fxx=0
+  , searchString = "TMP:2 m"
+  #, save_dir = "/n/wofsy_lab2/Users/emanninen/vprm/driver_data/hrrr_temperature"
+  , save_dir = "~/Downloads/temp"
+)#end Herbie
+
+HH_temperature 
