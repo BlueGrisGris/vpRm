3ae0c31dc568d0074e590ee08e5e09a2e02fbeab
BlueGrisGris8950 <emanninen@g.harvard.edu>
2022-08-09 06:03:44 +0000

    clean respir() comments

--- R/gee_respir.r
+++ R/gee_respir.r
@@ -9,15 +9,15 @@ gee <- function(lambda, Tscalar, Pscalar, Wscalar, EVI, PAR, PAR0){
 
 ### Winbourne et al 2021
 respir <- function(tair, alpha, beta, lc, isa, evi){
-	# respir_naive <- function(alpha, tair, beta){
+
 	respir_naive <- alpha * tair + beta 
-	#         return(respir_naive)
-	# }#end func respir
+
 	### TODO: ask Ian whats up with this madness
 	respir_het <- .5 * respir_naive * (1-isa)
 	respir_aut <- .5 * respir_naive * evi  
 	
 	respir <- respir_het + respir_aut
 
+	terra::names(respir) <- "respir"
 	return(respir)
 }#end func respir
