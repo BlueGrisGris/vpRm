629c835c76b6acc00a956365e7491c817b6cfa2f
BlueGrisGris <emanninen@g.harvard.edu>
2022-11-02 19:22:35 -0400

    download hrrr herbie in R with package reticulate

--- R/download_hrrr.r
+++ R/download_hrrr.r
@@ -1,3 +1,4 @@
+### TODO: I doubt this setup scheme is robust.  Can also direct user to herbie github
 #' setup_herbie  
 #' Set up herbie for downloading HRRR meteorology files. 
 #' conda is a prerequisite. 
@@ -18,28 +19,46 @@ setup_herbie <- function(){
 
 #' download_hrrr  
 #' download HRRR files using the herbie python.
-#' conda is a prerequisite. 
+#' To enable, must run vpRm::setup_herbie()
 #' herbie github: https://github.com/blaylockbk/Herbie 
 #'  
-#' @param times ()  
-#' @param vpRm (vpRm, character) An object of class vpRm, or a file path to a vpRm control file.
+#' @param times (POSIXct): 
+#' @param vpRm (vpRm, character): An object of class vpRm, or a file path to a vpRm control file.
 #'  
 #' @export
-download_hrrr <- function(times = NULL, vpRm = NULL){
+download_hrrr <- function(times = NULL, vpRm = NULL, download_dir = "."){
+	### use times from vpRm object if one is provided
 	if(!is.null(vpRm)){
 		if(class(vpRm) %in% c("character", "vpRm")){stop("parameter vpRm must be of class vpRm or path to control file of such")}
 		#                 TODO: if(class(vpRm) == "character"){read_vpRm()}
 		times <- vpRm$domain$time
 	}#end if(!is.null(vpRm)){
 
-	system("source activate herbie", ignore.stderr = T)
-	system(paste("python", system.file("fetch_scripts/hrrr_par_temp.py", package = "vpRm", mustWork = T), paste0("$",times)))
-	system("source deactivate herbie", ignore.stderr = T)
+	### don't even have to call the conda env?????
+	#         reticulate::use_condaenv("herbie")
+
+	herbie <- reticulate::import("herbie")
+	pd <- reticulate::import("pandas")
+
+	### this is unholy
+	### download temp
+	herbie$tools$fast_Herbie_download(
+		DATES = pd$to_datetime(times)
+		, model='hrrr'
+		, product='sfc'
+		, fxx=as.integer(0)
+		, searchString = "TMP:2 m"
+		, save_dir = file.path(download_dir, "hrrr_temp")
+	)#end fast_Herbie_download
+	### download par
+	herbie$tools$fast_Herbie_download(
+		DATES = pd$to_datetime(times)
+		, model='hrrr'
+		, product='sfc'
+		, fxx=as.integer(0)
+		, searchString = "DSWRF"
+		, save_dir = file.path(download_dir, "hrrr_par")
+	)#end fast_Herbie_download
+
+	return(NULL)
 }#end func download_hrrr
-
-times <- seq(as.POSIXct("2021-01-01 00:00:00"), as.POSIXct("2021-01-01 03:00:00"), by="hour")
-download_hrrr(times)
-
-# sink("~/Desktop/vprm.txt")
-# print(xx)
-# sink()
