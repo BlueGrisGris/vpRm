d7338f797f3711280c91cd9426c74e040739e999
BlueGrisGris <emanninen@g.harvard.edu>
2023-04-09 17:55:41 -0400

    switch landsat SR scale factor from collection 1 to C2

--- inst/fetch_tools/m2m_api_usgs/calc_evi.r
+++ inst/fetch_tools/m2m_api_usgs/calc_evi.r
@@ -1,14 +1,19 @@
 calc_evi <- function(scene, mission){
 	if(!mission %in% c("etm", "oli")){stop("mission must be one of etm, oli")} 
+	### apply landsat collection 2 surface reflectance scale factor
+	### https://www.usgs.gov/faqs/how-do-i-use-scale-factor-landsat-level-2-science-products
+	scene <- scene * 0.0000275  - .2
 	if(mission == "etm"){
-		evi <- 2.5*( (1e-4*scene["sr_b4"] - 1e-4*scene["sr_b3"])/(1e-4*scene["sr_b4"] + 6 * 1e-4*scene["sr_b3"] - 7.5*1e-4*scene["sr_b1"] + 1) )
-		evi[evi < -1 | evi > 1] <- NA
+		evi <- 2.5*( (scene["sr_b4"] - scene["sr_b3"])/(scene["sr_b4"] + 6 * scene["sr_b3"] - 7.5*scene["sr_b1"] + 1) )
+		evi[evi < -1] <- -1
+	       	evi[evi > 1] <- 1
 		names(evi) <- "EVI"
 		return(evi)
 	}#end if 
 	if(mission == "oli"){
-		evi <- 2.5*( (1e-4*scene["sr_b5"] - 1e-4*scene["sr_b4"])/(1e-4*scene["sr_b5"] + 6 * 1e-4*scene["sr_b4"] - 7.5*1e-4*scene["sr_b2"] + 1) )
-		evi[evi < -1 | evi > 1] <- NA
+		evi <- 2.5*( (scene["sr_b5"] - scene["sr_b4"])/(scene["sr_b5"] + 6 * scene["sr_b4"] - 7.5*scene["sr_b2"] + 1) )
+		evi[evi < -1] <- -1
+	       	evi[evi > 1] <- 1
 		names(evi) <- "EVI"
 		return(evi)
 	}#end if 
