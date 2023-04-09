7b130d37307ffcbf70349043fe3a1b03a7b20011
BlueGrisGris <emanninen@g.harvard.edu>
2023-04-09 14:32:07 -0400

    investigate why there is so many missing pixels

---
+++ inst/fetch_tools/m2m_api_usgs/examine_evi.r
@@ -0,0 +1,13 @@
+
+library(terra)
+
+evi_dir <- "/n/wofsy_lab2/Users/emanninen/vprm/driver_data/evi/weekly_evi"
+
+evi_scene_filename <- list.files(evi_dir, pattern = "proj", full.names = T)
+evi_week_filename <- list.files(evi_dir, pattern = "202225", full.names = T)
+
+evi_week <- rast(evi_week_filename)
+evi_scene <- rast(evi_scene_filename)
+plot(c(evi_week, evi_scene[[80]]))
+plot(!is.na(evi_week) & is.na(evi_scene[[80]]))
+
