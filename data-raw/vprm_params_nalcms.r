754a63e8c3f89d82cd1e815baaf3fafd1922ee53
BlueGrisGris <emanninen@g.harvard.edu>
2023-05-02 17:18:34 -0400

    fix agri LC setting lambda to zero

--- data-raw/vprm_params_nalcms.r
+++ data-raw/vprm_params_nalcms.r
@@ -1,3 +1,4 @@
+
 vprm_params_nalcms <- data.frame(
 	lc_code  = c(  "NA","ENFa","ENFb","ENFc","DBFa","DBFb", "MXF","SHBa","SHBb", "GRSa", "GRSb", "POLa","POLb", "POLc", "WET", "SOY", "BAR",  "URB",   "OTH",   "SNO")
 	, lc     = c(     0,     1,     2,     3,     4,     5,     6,     7,     8,      9,     10,     11,    12,     13,    14,    15,    16,     17,      18,    19  )
@@ -10,11 +11,5 @@ vprm_params_nalcms <- data.frame(
 	, alpha  = c(     0, 0.250, 0.250, 0.250, 0.271, 0.271, 0.244, 0.028, 0.028,  0.012,  0.028,  0.028, 0.028,  0.244, 0.081,  .209,     0,  0.250,   0.000, 0.000  )
 	, beta   = c(     0, 0.170, 0.170, 0.170, 0.250, 0.250, -0.24, 0.480, 0.480,  0.580,  0.720,  0.720, 0.720,  -0.24, 0.240,  .200,     0,  -0.24,   0.000, 0.000  )
 )#end data.frame()
-
-### check for each column
-# VPRM_OTH  == vprm_params[vprm_params$lc == "OTH",2:9] 
-
-### landcover dataset does not diff grs/svn or crp/soy
-vprm_params_nalcms <- vprm_params_nalcms[!vprm_params_nalcms$lc_code %in% c("SOY", "SVN"),]
 
 usethis::use_data(vprm_params_nalcms, overwrite = T)
